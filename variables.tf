5c5a11053335e938649eac528ce335ca6204b25f
JayyyJayyyyy <ebru.c.kavak@gmail.com>
2024-03-29 20:25:52 +0300

    files added

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "JayyyJayyyyy"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "cloudjourney.click"
+}
